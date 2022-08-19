6bcda5e24d522de5878b35548545a6dd7219e5d1
unknown <caio180205@gmail.com>
2022-08-19 17:49:31 -0300

    tentei fazer darktheme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import Nav from './components/Nav/Nav';
+import Perfil from './components/Pages/Perfil/Perfil';
 import Post from './components/Post/Post';
+import styled, { themeProvider } from 'styled-components';
+import { lightTheme, darkTheme, GlobalStyles } from './themes.js';
+
+const StyledApp = styled.div``;
 
 function App() {
+  const [theme,setTheme]= useState("dark");
+
+  const themeToggler = () => {
+    theme === "light"? setTheme("dark") : setTheme("light");
+    console.log(themeToggler)
+  
+  };
+
   return (
-    <div className="App">
-      <Nav />
-      <Post />
-    </div>
+    <themeProvider theme={theme === "light" ? "darkTheme" : "lightTheme"}>
+    <GlobalStyles />
+    <StyledApp>Hello World</StyledApp> <button onClick={() => themeToggler()}>Mudar tema</button>
+  
+    </themeProvider>
   );
 }
 
---
+++ src/components/Pages/Perfil/Perfil.js
@@ -0,0 +1,10 @@
+import "./Perfil.css";
+
+export default function Perfil(){
+    return(
+        <div className="perfilContainer">
+            <h1>Alguma coisa</h1>
+            <p>Alguma coisa</p> 
+        </div>
+    );
+}
